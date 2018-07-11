c55886fb1cf718a072ecd9b8775690e45c139cca
Eunbyul Cho <hello@eunbyulcho.me>
2018-07-11 16:22:20 -0700

    redux sprint

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,9 +4,42 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+
+/*
+var handleVideoSearch = (videos) => {
+  return (dispatch) => {
+    dispatch(changeVideo(videos[0]));
+    fetch(videos)
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            dispatch(changeVideo(videos[0]));
+            return response;
+        })
+        .then((response) => response.json())
+        .then((items) => dispatch(changeVideoList(items)))
+        .catch(() => dispatch(searchYouTube({ key: YOUTUBE_API_KEY, query: 'cats', max: 10 }, (response) => {})));
 };
+};
+*/
+
+
+
+
+var handleVideoSearch = (q) => {
+  return (dispatch) => {
+    searchYouTube({ key: YOUTUBE_API_KEY, query: q, max: 5 }, function(err,data) {
+      console.log(data)
+      if(err) {
+        console.log(err);
+      } else {
+         dispatch(changeVideoList(data));
+         dispatch(changeVideo(data[0]));
+      }
+    })
+
+  }
+}
 
 export default handleVideoSearch;
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,29 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
+const mapDispatchToProps = dispatch => ({
+  handleVideoListEntryTitleClick: (video) => dispatch(changeVideo(video))
+});
+const mapStateToProps = (state) => ({
+  videos: state.videoList
+});
+
+var VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
+
 
 export default VideoListContainer;
+
+//TODO: define a SearchContainer component which will hook up your action
+// dispatchers with your search component props.
+//HINT: use react-redux 'connect' method to generate a container component from
+//state and dispatch mappings.
+
+
+
+
+
+
